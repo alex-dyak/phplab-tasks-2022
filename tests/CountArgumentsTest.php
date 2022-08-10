613d7523aa39ef552433efe8815fa98a16a906ab
Alex <alexey.dyakonov@catenamedia.com>
2022-08-10 16:54:42 +0300

    [PHP-functions] Create CountArgumentsTest test

---
+++ tests/CountArgumentsTest.php
@@ -0,0 +1,58 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class CountArgumentsTest extends TestCase
+{
+    protected $functions;
+
+    protected function setUp(): void
+    {
+        $this->functions = new functions\Functions();
+    }
+
+    /**
+     * @dataProvider NoArgumentsDataProvider
+     */
+    public function testNoArguments($arg, $expected)
+    {
+        $this->assertEquals($expected, $this->functions->countArguments());
+    }
+
+    /**
+     * @dataProvider OneStringArgumentDataProvider
+     */
+    public function testOneStringArgument($arg, $expected)
+    {
+        $this->assertEquals($expected, $this->functions->countArguments($arg));
+    }
+
+    /**
+     * @dataProvider TwoStringArgumentsDataProvider
+     */
+    public function testTwoStringArguments($arg1, $arg2, $expected)
+    {
+        $this->assertEquals($expected, $this->functions->countArguments($arg1, $arg2));
+    }
+
+    public function NoArgumentsDataProvider(): array
+    {
+        return [
+            ['', ['argument_count' => 0, 'argument_values' => []]]
+        ];
+    }
+
+    public function OneStringArgumentDataProvider(): array
+    {
+        return [
+            ['Hello', ['argument_count' => 1, 'argument_values' => ['Hello']]]
+        ];
+    }
+
+    public function TwoStringArgumentsDataProvider(): array
+    {
+        return [
+            ['Hello', 'Jack', ['argument_count' => 2, 'argument_values' => ['Hello', 'Jack']]]
+        ];
+    }
+}
